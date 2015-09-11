2c06522ba730145797ff69ea5efaf2a4a851fb60
Anil <anil77k@gmail.com>
2015-09-11 12:07:49 -0700

    Added second test with priority 2

--- WebdriverTest/src/test/java/example/NewTest.java
+++ WebdriverTest/src/test/java/example/NewTest.java
@@ -2,6 +2,7 @@ package example;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
@@ -11,12 +12,20 @@ import org.testng.annotations.Test;
 
 public class NewTest {
 	private WebDriver driver;
-  @Test
-  public void testEasy() {
+  @Test(priority =1)
+  public void getTitle() {
 	  driver.get("http://www.guru99.com/selenium-tutorial.html");
 	  String title=driver.getTitle();
 	  System.out.println("Title is: "+title);
 	  Assert.assertTrue(title.contains("Free Selenium Tutorials"));
+  }
+  
+  @Test (priority=2)
+  public void goHome(){
+	  driver.findElement(By.ByXPath.xpath(".//*[@id='rt-header']/div/div[2]/div[1]/ul/li[1]/a")).click();
+	  System.out.println(driver.getTitle());
+	  Assert.assertTrue(driver.getTitle().contains("Meet Guru99"));
+	  
   }
   @BeforeTest
   public void beforeTest() {
